7b31802dc8ad01ab53bbe77f1e900d80efa59186
MelodicAlbuild <ralexdrum@gmail.com>
2024-10-28 13:01:40 -0500

    fix: extras link was incorrect

--- utils/data/extras.js
+++ utils/data/extras.js
@@ -3,7 +3,7 @@ export const extras = [
     id: 1,
     title: "JetBrains GameDev Days 2024",
     duration: "October 10th, 2024",
-    link: "https://www.youtube.com/live/0YVD9Zuei80?si=YSD3fRb18twdFaXV&t=21544",
+    link: "https://youtu.be/i8MVVVdGPGE",
     linkName: "Watch Here",
   },
 ];
